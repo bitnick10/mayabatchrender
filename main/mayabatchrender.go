c20a32ba77644cb96a0d5bd89b373954f3c0e68d
Ginseng <bitnick10@gmail.com>
2016-01-04 12:35:00 +0800

    c

--- main/mayabatchrender.go
+++ main/mayabatchrender.go
@@ -2,15 +2,16 @@ package main
 
 import (
 //"github.com/axgle/mahonia"
-	"os/exec"
 	"fmt"
+	"os/exec"
 //"bytes"
-	"time"
 	"os"
+	"time"
 //"io/ioutil"
 	"strconv"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
-
 
 //Common flags for the command line renderer
 //https://knowledge.autodesk.com/support/maya/learn-explore/caas/CloudHelp/cloudhelp/2016/ENU/Maya/files/GUID-0280AB86-8ABE-4F75-B1B9-D5B7DBB7E25A-htm.html
@@ -18,31 +19,35 @@ import (
 //Render from the command line using mental ray
 //https://knowledge.autodesk.com/support/maya/learn-explore/caas/CloudHelp/cloudhelp/2016/ENU/Maya/files/GUID-FCB01DEF-10DF-4222-A047-AF0122782397-htm.html
 
-//convert "E:\My\Desktop\Pal2\art\maya\Songzhou Outskirts\images\Outskirts.png" -gamma 2.2 "E:\My\Desktop\Pal2\art\maya\Songzhou Outskirts\images\Outskirtsgammad.png"
-
-//"C:\Program Files\Autodesk\Maya2016\bin\Render.exe" -r mr -proj "E:\My\Desktop\Pal2\art\maya\Songzhou Outskirts" -cam "light_AD:camera45gate" -of "png" "E:\My\Desktop\Pal2\art\maya\Songzhou Outskirts\scenes\Outskirts.mb"
 type RenderSettings struct {
-	ProjectPath string
-	SceneName   string
-	CameraName  string
-	ImageName   string
-	PercentResolution int // 50 means 50%
+	ProjectPath       string `yaml:"ProjectPath"`
+	SceneName         string `yaml:"SceneName"`
+	CameraName        string `yaml:"CameraName"`
+	ImageName         string
+	PercentResolution int `yaml:"PercentResolution"` // 50 means 50%
 }
 
 func main() {
 	rs := RenderSettings{}
-	rs.ProjectPath = "E:\\My\\Desktop\\Pal2\\art\\maya\\Songzhou Outskirts"
-	rs.SceneName = "Outskirts"
-	rs.CameraName = "light_AD:camera45gate"
+	data, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Println("error: %v", err)
+	}
+	err = yaml.Unmarshal([]byte(data), &rs)
+	if err != nil {
+		fmt.Println("error: %v", err)
+	}
+
 	rs.ImageName = rs.SceneName + " " + time.Now().Format("2006-01-02 150405")
-	rs.PercentResolution = 100
+	fmt.Printf("%+v\n", rs)
 
 	RenderCommand(rs)
 	ConvertCommand(rs.ImageName)
+	ConvertCommand2(rs.ImageName)
+
 	var enterToExit string
 	fmt.Println("press Enter to exit")
 	fmt.Scanf(enterToExit)
-	//fmt.Println(string(out))
 }
 
 //var printedOut bytes.Buffer
@@ -57,28 +62,44 @@ func main() {
 //		//}
 //	//}
 //}
+
+// gamma 2.2
 func ConvertCommand(imageName string) {
+	program := "ImageMagick-6.9.3-0-portable-Q16-x64/convert.exe"
 	dir, _ := os.Getwd()
 	idir := dir + "/images"
 	imageFullName := idir + "/" + imageName + ".png"
-	cmd := exec.Command("convert",
+	cmd := exec.Command(program,
 		imageFullName,
 		"-gamma", "2.2",
-		imageFullName);
+		imageFullName)
+	CommandRun(cmd)
+}
+
+// png to jpg
+func ConvertCommand2(imageName string) {
+	program := "ImageMagick-6.9.3-0-portable-Q16-x64/convert.exe"
+	dir, _ := os.Getwd()
+	idir := dir + "/images"
+	imageFullNameWithoutSuffix := idir + "/" + imageName
+	cmd := exec.Command(program,
+		imageFullNameWithoutSuffix + ".png",
+		"-quality", "92",
+		imageFullNameWithoutSuffix + ".jpg")
 	CommandRun(cmd)
 }
 
 func RenderCommand(rs RenderSettings) {
 	dir, _ := os.Getwd()
-	render := "C:/Program Files/Autodesk/Maya2016/bin/Render.exe"
+	program := "C:/Program Files/Autodesk/Maya2016/bin/Render.exe"
 	//imageName := rs.SceneName + " " + time.Now().Format("2006-01-02 150405")
 
-	cmd := exec.Command(render,
+	cmd := exec.Command(program,
 		"-r", "mr",
 		"-rd", dir + "/images",
 		"-im", rs.ImageName,
 		"-of", "png",
-		"-percentRes",strconv.Itoa(rs.PercentResolution),
+		"-percentRes", strconv.Itoa(rs.PercentResolution),
 		"-proj", rs.ProjectPath,
 		"-cam", rs.CameraName,
 		rs.ProjectPath + "/scenes/" + rs.SceneName + ".mb")
