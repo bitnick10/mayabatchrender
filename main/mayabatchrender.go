50987601b1c8f4f6100783f666b31ef4618a9bb1
Ginseng <bitnick10@gmail.com>
2016-01-05 14:01:51 +0800

    c

--- main/mayabatchrender.go
+++ main/mayabatchrender.go
@@ -1,23 +1,20 @@
 package main
 
+// Common flags for the command line renderer
+// https://knowledge.autodesk.com/support/maya/learn-explore/caas/CloudHelp/cloudhelp/2016/ENU/Maya/files/GUID-0280AB86-8ABE-4F75-B1B9-D5B7DBB7E25A-htm.html
+
+// Render from the command line using mental ray
+// https://knowledge.autodesk.com/support/maya/learn-explore/caas/CloudHelp/cloudhelp/2016/ENU/Maya/files/GUID-FCB01DEF-10DF-4222-A047-AF0122782397-htm.html
+
 import (
-//"github.com/axgle/mahonia"
 	"fmt"
 	"os/exec"
-//"bytes"
 	"os"
 	"time"
-//"io/ioutil"
 	"strconv"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 )
-
-//Common flags for the command line renderer
-//https://knowledge.autodesk.com/support/maya/learn-explore/caas/CloudHelp/cloudhelp/2016/ENU/Maya/files/GUID-0280AB86-8ABE-4F75-B1B9-D5B7DBB7E25A-htm.html
-
-//Render from the command line using mental ray
-//https://knowledge.autodesk.com/support/maya/learn-explore/caas/CloudHelp/cloudhelp/2016/ENU/Maya/files/GUID-FCB01DEF-10DF-4222-A047-AF0122782397-htm.html
 
 type RenderSettings struct {
 	ProjectPath       string `yaml:"ProjectPath"`
@@ -50,19 +47,6 @@ func main() {
 	fmt.Scanf(enterToExit)
 }
 
-//var printedOut bytes.Buffer
-//var cmdOut bytes.Buffer
-//
-//func PrintOutRealtime() {
-//	//for {
-//		//time.Sleep(time.Millisecond * 100)
-//		//if 0 != cmdOut.Len() {
-//			//fmt.Print(cmdOut.String())
-//			//cmdOut.Reset()
-//		//}
-//	//}
-//}
-
 // gamma 2.2
 func ConvertCommand(imageName string) {
 	program := "ImageMagick-6.9.3-0-portable-Q16-x64/convert.exe"
@@ -92,7 +76,6 @@ func ConvertCommand2(imageName string) {
 func RenderCommand(rs RenderSettings) {
 	dir, _ := os.Getwd()
 	program := "C:/Program Files/Autodesk/Maya2016/bin/Render.exe"
-	//imageName := rs.SceneName + " " + time.Now().Format("2006-01-02 150405")
 
 	cmd := exec.Command(program,
 		"-r", "mr",
@@ -104,16 +87,12 @@ func RenderCommand(rs RenderSettings) {
 		"-cam", rs.CameraName,
 		rs.ProjectPath + "/scenes/" + rs.SceneName + ".mb")
 
-	//cmd := exec.Command("D:\\projects\\tickers.exe")
 	startTime := time.Now()
 	CommandRun(cmd)
 	endTime := time.Now()
 	dur := endTime.Sub(startTime)
 	fmt.Print(dur.Minutes())
 	fmt.Println(" minitues")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
 }
 func CommandRun(cmd *exec.Cmd) {
 	fmt.Println("cmd.Path = " + cmd.Path)
@@ -127,17 +106,9 @@ func CommandRun(cmd *exec.Cmd) {
 		fmt.Println("err")
 		fmt.Println(err)
 	}
-	//go PrintOutRealtime()
+
 	err = cmd.Wait()
 	fmt.Printf("Command finished with error: %v\n", err)
-
-	//fmt.Println(cmdOut.String())
-
-	//write to file to read
-	//f, err := os.Create("out.txt")
-	//defer f.Close()
-	//f.WriteString(os.Stdout. cmdOut.String())
-
 }
 
 func StringAddDoubleQuotes(str string) string {
